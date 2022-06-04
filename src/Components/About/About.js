95d8f01f84156d9cefd803a5ac29a03ad331cf39
Lipatov <lipatovnn@yahoo.com>
2022-06-04 19:24:18 +0300

    anchor that leads to About component; minor improvements

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -19,15 +19,29 @@ const About = ({aboutVisible, aboutNotVisible}) => {
                 <img className="profilePicture aboutPhotoContainer" src={AboutPicture}/>
             </div>
             <div className="TextContainer">
+                <h4>
+                    My name is <span>Nikita</span>
+                </h4>
                 <span>
-                    My name is Nikita
+                    I'm a fullstack web developer. My first application was a desktop software for searching an unique taxpayer ID in open government tax service web-site.
                 </span>
-                <span>
-                Lorem, Ipsum Dolor Sit Amet Consectetur Adipisicing Elit. 
-                Recusandae Aut Adipisci Necessitatibus Veniam, Provident Vero Architecto 
-                Praesentium, Non Sequi Libero Corrupti Molestias. Architecto 
-                Voluptate Eligendi Maiores, Error Magni Accusamus Officia.
-                </span>
+                <div className="BottomText">
+                    <h4>
+                        Age: <span>24</span>
+                    </h4>
+                    <h4>
+                        Language: <span>English, Russian</span>
+                    </h4>
+                    <h4>
+                        City: <span>Klin, Moscow Region</span>
+                    </h4>
+                    <h4>
+                        Major roles: <span>Fullstack web-developer</span>
+                    </h4>
+                    <h4>
+                        Minor roles: <span>Mobile and Destop developer, DevOps</span>
+                    </h4>
+                </div>
             </div>
         </div>
     )
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,13 @@
 import "./Home.css";
 import profilePicture from "../../img/photo5463225416312535627.jpg";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAngleDoubleDown } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDoubleDown } from '@fortawesome/free-solid-svg-icons';
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 import React, {useEffect, useState} from "react";
 
 const Home = ({setAboutVisibility}) => {
-    const downArrows = <FontAwesomeIcon icon={faAngleDoubleDown} />
+    const userIcon = <FontAwesomeIcon icon={faUser} style={{"fontSize": "21pt" }}/>
+    const downArrowsIcon = <FontAwesomeIcon icon={faAngleDoubleDown} />
     useEffect(()=>{
         setAboutVisibility.current = aboutVisible;
     }, []);
@@ -29,13 +31,21 @@ const Home = ({setAboutVisibility}) => {
                         Web Developer
                     </span>
                 </div>
+                <span style={{fontSize: "11pt", marginTop: "10px"}}>
+                        Creative Web Developer Offering 2+ Years Of Experience Providing High-Impact Web Solutions For Many Different Organizations.
+                </span>
+                <a href="#aboutAnchor" className="btn">
+                    More info about me
+                    {userIcon}
+                </a>
             </div>
             <div className="PhotoContainer">
                 <img className="profilePicture" src={profilePicture}/>
             </div>
             <div className="downArrowsDiv">
-                {!isaboutVisible ? downArrows : null}
+                {!isaboutVisible ? downArrowsIcon : null}
             </div>
+            <div id="aboutAnchor" style={{position: "absolute", bottom: "0"}}/>
         </div>
     )
 }
