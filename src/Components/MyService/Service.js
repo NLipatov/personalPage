534d545195d26a38599f1b997aa2ee7a4817aa98
Lipatov <lipatovnn@yahoo.com>
2022-06-06 20:28:48 +0300

    service component

--- src/Components/MyService/Service.js
+++ src/Components/MyService/Service.js
@@ -1,12 +1,45 @@
 import "./Service.css";
 import React from "react";
+import ServiceItem from "./ServiceItem";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faCode } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLaptopCode } from "@fortawesome/free-solid-svg-icons";
+import { faDatabase } from "@fortawesome/free-solid-svg-icons";
+import { faServer } from "@fortawesome/free-solid-svg-icons";
 
 const Service = () => {
+    const backendIcon = <FontAwesomeIcon icon={faCode} />
+    const frontendIcon = <FontAwesomeIcon icon={faLaptopCode} />
+    const databaseIcon = <FontAwesomeIcon icon={faDatabase} />
+    const devOpsIcon = <FontAwesomeIcon icon={faServer} />
+    const userIcon = <FontAwesomeIcon icon={faUser} style={{fontSize: "21pt" }}/>
+    const ServiceList = [
+        {
+            name: "BackEnd",
+            icon: backendIcon
+        },
+        {
+            name: "Frontend",
+            icon: frontendIcon
+        },
+        {
+            name: "Database Engineering",
+            icon: databaseIcon
+        },
+        {
+            name: "DevOps",
+            icon: devOpsIcon
+        }
+
+    ]
     return (
-        <div>
-            <span>
-                I'm Service
-            </span>
+        <div className="ServiceDiv">
+            <div className="ServiceItems">
+                {ServiceList.map((service) => {
+                    <ServiceItem userIcon={service.icon}/>
+                })}
+            </div>
         </div>
     )
 
---
+++ src/Components/MyService/ServiceItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ServiceItem = ({userIcon}) => {
+    return (
+        <div>
+            {userIcon}
+        </div>
+    )
+
+}
+
+export default ServiceItem;
