e301ddb8276efdee8aa51b2e755553c14ea3d385
Nikita Lipatov <nlipatov@dynamicsun.ru>
2022-06-20 10:03:32 +0300

    Portfolio redesign

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -3,25 +3,26 @@ import "./Portfolio.css";
 import { ReactPhotoCollage } from "react-photo-collage";
 import React from "react";
 
-const setting = {
-    width: '70%',
-    height: ['450px', '270px'],
-    layout: [1, 4],
-    photos: [
-      { source: 'https://i.ibb.co/dWW4Qmt/Screenshot-2022-06-14-134708.png' },
-      { source: 'https://i.ibb.co/McLfY5V/Screenshot-2022-06-14-134708.png' },
-      { source: 'https://i.ibb.co/CV47YJk/Screenshot-2022-06-14-134708.png' },
-    ],
-    showNumOfRemainingPhotos: true
-  };
+// const setting = {
+//     width: '70%',
+//     height: ['450px', '270px'],
+//     layout: [1, 4],
+//     photos: [
+//       { source: 'https://i.ibb.co/dWW4Qmt/Screenshot-2022-06-14-134708.png' },
+//       { source: 'https://i.ibb.co/McLfY5V/Screenshot-2022-06-14-134708.png' },
+//       { source: 'https://i.ibb.co/CV47YJk/Screenshot-2022-06-14-134708.png' },
+//     ],
+//     showNumOfRemainingPhotos: true
+//   };
 
 const Portfolio = () => {
     return (
         <div className="PortfolioSection visual_component" id="portfolio">
             <Declarator DeclaratorValue={"Portfolio"}/>
-            <div className="PhotoCollage">
+            {/* <div className="PhotoCollage">
                 <ReactPhotoCollage {...setting} />
-            </div>
+            </div> */}
+            
         </div>
     )
 }
---
+++ src/Components/Portfolio/PortfolioItem/PortfolioItem.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./PortfolioItem.css";
+
+const PortfolioItem = () => {
+    return( 
+        <div>
+
+        </div>
+    )
+}
+
+export default PortfolioItem;
