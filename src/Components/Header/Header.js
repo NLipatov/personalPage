07a46b4e38a76184930bb5960e1bf51515946341
Nikita Lipatov <nlipatov@dynamicsun.ru>
2022-06-20 21:49:10 +0300

    animation for menu-burger icon

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,6 +1,24 @@
 import "./Header.css";
 
 const Header = () => {
+    let crossAnimationPlayed = false;
+    const onCrossNavBarClick = () => {
+        if(!crossAnimationPlayed){
+            document.querySelectorAll(".CrossNavBar span")[0].style.display = "none";
+            document.querySelectorAll(".CrossNavBar span")[1].style.transform = "rotate(45deg)";
+            document.querySelectorAll(".CrossNavBar span")[2].style.transform = "rotate(-45deg)";
+            document.querySelectorAll(".CrossNavBar span")[2].style.marginTop = "-10px";
+            crossAnimationPlayed = true;
+        }
+        else{
+            document.querySelectorAll(".CrossNavBar span")[0].style.display = "";
+            document.querySelectorAll(".CrossNavBar span")[1].style.transform = "rotate(90deg)";
+            document.querySelectorAll(".CrossNavBar span")[2].style.transform = "rotate(90deg)";
+            document.querySelectorAll(".CrossNavBar span")[2].style.marginTop = "10px";
+            crossAnimationPlayed = false;
+        }
+
+    };
     return(
         <div className="Header">
             <div className="HeaderContent">
@@ -13,6 +31,13 @@ const Header = () => {
                     <a href="#experience" id="NavBarExperienceLink">Experience</a>
                     <a href="#portfolio" id="NavBarPortfolioLink">Portfolio</a>
                     <a href="#contact" id="NavBarContactLink">Contact</a>
+                </div>
+                <div 
+                    className="CrossNavBar"
+                    onClick={onCrossNavBarClick}>
+                    <span/>
+                    <span/>
+                    <span/>
                 </div>
             </div>
 
